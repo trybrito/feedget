0cffeecd685f16090f648459f99da3958a941214
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2022-05-03 19:37:43 -0300

    chore(project): initial setup

---
+++ web/src/App.tsx
@@ -0,0 +1,6 @@
+import Widget from "./components/Widget";
+import "./styles/global.css";
+
+export default function App() {
+  return <Widget />;
+}
---
+++ web/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{tsx, css}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: "#8257e6"
+        }
+      }
+    },
+  },
+  plugins: [],
+};
